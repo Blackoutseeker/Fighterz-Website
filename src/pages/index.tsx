d69bc037d0d356a80362b6c990674f0e885432ee
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-09 04:10:10 -0300

    feat(page): add Frame component to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,16 @@ import { technologies } from '@/utils/constants'
 import Slide from '@/components/Slide'
 import TechCard from '@/components/TechCard'
 import Viewport from '@/components/Viewport'
+import Frame from '@/components/Frame'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const frameImages: string[] = []
+
+for (let i = 0; i < 4; i++) {
+  frameImages.push(`/assets/sprites/frames/${i}.png`)
+}
 
 export default function Home() {
   return (
@@ -18,7 +26,7 @@ export default function Home() {
         className={`flex min-h-screen flex-col items-center p-4 md:p-20 ${inter.className}`}
       >
         <Slide>
-          <p className="text-xl font-bold md:text-4xl">
+          <p className="text-center text-xl font-bold md:text-4xl">
             Tecnologias utilizadas:
           </p>
           <ul className="my-10 flex max-w-full flex-wrap justify-center gap-6 md:max-w-full md:gap-10 lg:max-w-[80%]">
@@ -28,10 +36,29 @@ export default function Home() {
           </ul>
         </Slide>
         <Slide>
-          <h2 className="my-10 text-xl font-bold md:text-4xl">
+          <h2 className="my-10 text-center text-xl font-bold md:text-4xl">
             Funcionamento da Viewport (FitViewport)
           </h2>
           <Viewport />
+        </Slide>
+        <Slide>
+          <h2 className="my-10 text-center text-xl font-bold md:text-4xl">
+            Iteração de colisões
+          </h2>
+          <ul className="flex flex-col gap-5 lg:flex-row">
+            {frameImages.map(image => (
+              <Frame key={image} image={image} />
+            ))}
+          </ul>
+          <div className="mt-16 rounded-md p-2 hover:shadow-md">
+            <Image
+              className="size-[240px]"
+              src="/assets/sprites/gifs/punch.gif"
+              width={100}
+              height={100}
+              alt="Ryu punching"
+            />
+          </div>
         </Slide>
       </main>
     </>
