395b8de6e96066e02764edbdd39d7ba6e0691b1d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-09 01:19:08 -0300

    chore(_document): add page description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Fighterz é um jogo de luta 2D inspirado nas franquias de Street Fighter e Guilty Gear."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
