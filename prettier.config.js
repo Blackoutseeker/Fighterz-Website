e23a4f31a414dedcb8a935b000d41b6ebee85574
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-09 01:11:08 -0300

    chore(prettier): create Prettier configuration file

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss']
+}
