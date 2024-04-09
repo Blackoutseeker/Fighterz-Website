4127fa5f38b0e7f9d92a43f58aa16f7c2c6ff954
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-09 03:00:39 -0300

    feat(utils): create technologies constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+export const technologies: string[] = [
+  'Java',
+  'libGDX',
+  'Aseprite',
+  'Git',
+  'GitHub',
+  'Draw.io',
+  'IntelliJ IDEA',
+  'Gradle',
+  'Photoshop',
+  'Audacity',
+  'Tinify'
+]
